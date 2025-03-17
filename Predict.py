a44cb307d22e7c1921644046328532ff89f55655
Ghilas Tidjet <ghilastidjet7@gmail.com>
2025-03-17 18:04:03 +0100

    déclaration de l'exception NonValide

--- Predict.py
+++ Predict.py
@@ -15,4 +15,12 @@ def getsoup(url):
     soup = BeautifulSoup(response.text, "html.parser")
     return soup
 
+class NonValide(Exception):
+    """
+    Exception levée lorsqu'une annonce ne correspond pas aux critères requis.
+    Cette exception sera utilisée pour signaler les annonces à éliminer, comme 
+    les parkings, fonds de commerce, ou annonces trop peu chères.
+    """
+    pass
+
 print(getsoup("https://www.seloger.com/annonces/achat/appartement/paris-17eme-75/monceau-wagram/157073189.htm"))
